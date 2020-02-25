44f754c7523419962232b793b61d96a87e2259e7
David Lau <David.lau@codeimmersives.com>
2020-02-25 14:26:54 -0500

    finished

--- main.js
+++ main.js
@@ -1,11 +1,39 @@
 /******************
  * YOUR CODE HERE *
  ******************/
+onlyOdds = (x) => {
+const array = []
+for (let i = 0 ; x.length > i ; i++){
+if (x[i]%2 !== 0 )
+array.push(x[i])
+}return array
+}
 
+onlyEvens = (x) => {
+  const array = []
+for (let i = 0 ; x.length > i ; i++){
+ if (x[i]%2 === 0)
+ array.push(x[i])
 
+} return array
+}
 
+shortNamesOnly = (x) => {
+  const array = []
+  for (let i = 0 ; x.length > i ; i++){
+if (x[i].length < 7)
+array.push(x[i])
+  }return array
+}
 
+dNames = (x) => {
+  const array = []
+for (let i = 0 ; x.length > i ; i++){
+if (x[i].startsWith('D'))
+array.push(x[i])
+}return array
 
+}
 
 /*********************************
  * OUR CODE BELOW; DO NOT TOUCH! *
